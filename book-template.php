9a265e787fe444b28481a00b75d083959b539290
ulaulaman <34242086+ulaulaman@users.noreply.github.com>
2018-01-25 12:42:10 +0100

    Message metabox on side

--- book-template.php
+++ book-template.php
@@ -2,10 +2,11 @@
 /*
 Plugin Name: Book template
 Description: Plugin per l'inserimento dei dati editoriali di un libro o di un fumetto con uno shortcode. Al momento solo in italiano.
-Version: 0.4
+Version: 0.5
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
 Plugin URI: https://github.com/ulaulaman/book-template
+License: GPLv2 or later
 */
 /* ------------------------------------------------------ */
 # ---------------------------------------------------------
@@ -23,19 +24,39 @@ Plugin URI: https://github.com/ulaulaman/book-template
 # 0.2.2 nomi attributi: dall'italiano all'inglese
 # 0.3 ottimizzazione codice shortcode
 # 0.4 aggiunta box con messaggio per l'uso dello shortcode in cima al post
+# 0.5 spostamento del metabox di messaggio sulla colonna destra
 #
 # Aggiunta metabox
-add_action( 'load-post.php', 'bookdata_meta_box' );
-add_action( 'load-post-new.php', 'bookdata_meta_box' );
+add_action( 'load-post.php', 'bookdata_meta_box_setup' );
+add_action( 'load-post-new.php', 'bookdata_meta_box_setup' );
 
-# Definizione metabox
-function bookdata_meta_box() {
-  ?>
-  <div class="update-nag notice">
-      <p><?php _e( 'Esempio generico [bookdata title="Titolo" author="Autori" publisher="Editore" date="Data" pages ="numero pagine" type="Borssurato,Cartonato,Digitale/on-line" price="prezzo valuta/gratuito"]<br/>In caso di fumetto o libro illustrato, inserire il colore [bookdata ... col="colore,b/n"]<br/>Possono essere inseriti opzionalmente ISBN [bookdata ... isbn="codice"] o ISSN [bookdata ... issn="codice"], il traduttore [bookdata ... translator="Traduttore"] ed eventuali note aggiuntive [dati_editoriali ... notes="Note aggiuntive"]<br/>I dati possono essere inseriti anche in maniera disordinata: ci penserà il plugin a riordinarli', 'book_template_textdomain' ); ?></p>
-  </div>
-  <?php
+/* Meta box setup function. */
+function bookdata_meta_box_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'bookdata_meta_box' );
 }
+
+function bookdata_meta_box() {
+
+  add_meta_box(
+    'bookdata-post-class',      // Unique ID
+    esc_html__( 'Inserimento dati editoriali', 'example' ),    // Title
+    'bookdata_class_meta_box',   // Callback function
+    'post',         // Admin page (or post type)
+    'side',         // Context
+    'high'         // Priority
+  );
+}
+
+/* Display the post meta box. */
+function bookdata_class_meta_box( $post ) { ?>
+
+  <?php wp_nonce_field( basename( __FILE__ ), 'bookdata_class_nonce' ); ?>
+
+  <p>
+    <label for="bookdata-post-class">Esempio generico [bookdata title="Titolo" author="Autori" publisher="Editore" date="Data" pages ="numero pagine" type="Borssurato,Cartonato,Digitale/on-line" price="prezzo valuta/gratuito"]<br/>In caso di fumetto o libro illustrato, inserire il colore [bookdata ... col="colore,b/n"]<br/>Possono essere inseriti opzionalmente ISBN [bookdata ... isbn="codice"] o ISSN [bookdata ... issn="codice"], il traduttore [bookdata ... translator="Traduttore"] ed eventuali note aggiuntive [dati_editoriali ... notes="Note aggiuntive"]<br/>I dati possono essere inseriti anche in maniera disordinata: ci penserà il plugin a riordinarli.</label></p>
+<?php }
 
 # Creazione shortcode dati editoriali
 add_shortcode('bookdata', 'bookdata');
