35b6ae61e6474fa60e9cb598bbe8028bc0e00caa
ulaulaman <gianluigi.ulaula@gmail.com>
2020-08-13 11:40:40 +0200

    Aggiunto allo shortcode il campo dell'età consigliata

--- book-template.php
+++ book-template.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Book Template
 Description: Plugin che aggiunge uno shortcode per la creazione di un box con i dati editoriali di un libro o di un fumetto.
-Version: 2020.0718.1
+Version: 2020.0813
 Author: Gianluigi Filippelli
 Author URI: http://dropseaofulaula.blogspot.it/
 Plugin URI: https://ulaulaman.github.io/book-template/
@@ -46,7 +46,7 @@ function bookdata_class_meta_box( $post ) { ?>
   <?php wp_nonce_field( basename( __FILE__ ), 'bookdata_class_nonce' ); ?>
 
   <p>
-    <label for="bookdata-post-class"><?php _e( 'Esempio generico [bookdata title="Titolo" author="Autore/i" publisher="Editore" date="Data" pages="numero pagine" type="brossurato,cartonato,digitale/on-line" price="prezzo/gratuito"]', 'book-template' ); ?><br/><?php _e(' In caso di fumetto o libro illustrato, inserire il colore [bookdata ... col="colore,b/n"]', 'book-template' ); ?><br/><?php _e( 'Possono essere inseriti opzionalmente ISBN [bookdata ... isbn="codice"] o ISSN [bookdata ... issn="codice"], il traduttore [bookdata ... translator="Traduttore"] ed eventuali note aggiuntive [bookdata ... notes="Note aggiuntive"]', 'book-template' ); ?><br/><?php _e( 'I dati possono essere inseriti anche in maniera disordinata: ci penserà il plugin a riordinarli.', 'book-template' ); ?></label></p>
+    <label for="bookdata-post-class"><?php _e( 'Esempio generico [bookdata title="Titolo" author="Autore/i" publisher="Editore" date="Data" pages="numero pagine" type="brossurato,cartonato,digitale/on-line" price="prezzo/gratuito"]', 'book-template' ); ?><br/><?php _e(' In caso di fumetto o libro illustrato, inserire il colore [bookdata ... col="colore,b/n"]', 'book-template' ); ?><br/><?php _e( 'Possono essere inseriti opzionalmente ISBN [bookdata ... isbn="codice"] o ISSN [bookdata ... issn="codice"], il traduttore [bookdata ... translator="Traduttore"], l\'età consigliata  [bookdata ... age="età consigliata"] ed eventuali note aggiuntive [bookdata ... notes="Note aggiuntive"]', 'book-template' ); ?><br/><?php _e( 'I dati possono essere inseriti anche in maniera disordinata: ci penserà il plugin a riordinarli.', 'book-template' ); ?></label></p>
 <?php }
 
 # creazione shortcode dati editoriali
@@ -57,18 +57,19 @@ add_shortcode( 'bookdata', 'bookdata' );
    extract(
       shortcode_atts(
          array( 
-		'title' => null, 
-		'author' => null, 
+		'title' => null,
+		'author' => null,
 		'translator' => null,
-		'publisher' => null, 
-		'date' => null, 
-		'pages' => null, 
-		'type' => null, 
-		'col' => null, 
-		'price' => null, 
-		'isbn' => null, 
-		'issn' => null, 
-                'notes' => null,
+		'publisher' => null,
+		'date' => null,
+		'pages' => null,
+		'type' => null,
+		'col' => null,
+    'price' => null,
+    'age' => null,
+		'isbn' => null,
+		'issn' => null,
+    'notes' => null,
 	 ),
          $atts
       )
@@ -90,6 +91,11 @@ add_shortcode( 'bookdata', 'bookdata' );
    {$book = $book.'<br/>'.$publisher.', '.$date.'<br/>'.$pages.', '.$type.', '.$col.' – '.$price;}
    else
    {$book = $book.'<br/>'.$publisher.', '.$date.'<br/>'.$pages.', '.$type.' – '.$price;}
+
+   if ( $age <> null )
+   {$book = $book.'<br/>'.__( 'Lettura consigliata per ', 'book-template' ).$age;}
+   else
+   {$book = $book;}
 
    if ( $isbn <> null )
    {$book = $book.'<br/>ISBN: '.$isbn;}
